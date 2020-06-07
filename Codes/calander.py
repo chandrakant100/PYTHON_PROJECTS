8056be7af66a123c63135a922c6be12ea1b62e6d
chandrakant100 <chandrakant.barcelona@gmail.com>
2020-06-07 21:42:26 +0530

    added modular codes

---
+++ Codes/calander.py
@@ -0,0 +1,8 @@
+import calendar
+"""print(calendar.weekheader(3))
+print(calendar.firstweekday())
+print()
+
+print(calendar.month(2020,4))"""
+n=int(input(":---Enter the year to get the calender:--\n"))
+print(calendar.calendar(n))
