70ab1c4342904bc54ef77531897eea5779750a4f
Chandrakant jena <chandrakant.barcelona@gmail.com>
2020-06-26 21:59:16 +0530

    Minor changes

--- Codes/JARVIS.py
+++ Codes/JARVIS.py
@@ -101,9 +101,9 @@ if __name__ == "__main__":
             try:
                 speak("What should I say?")
                 content = takeCommand()
-                to = "harryyourEmail@gmail.com"    
+                to = "yourEmail@gmail.com"    
                 sendEmail(to, content)
                 speak("Email has been sent!")
             except Exception as e:
                 print(e)
-                speak("Sorry my friend harry bhai. I am not able to send this email")    
+                speak("Sorry chandrakant, I am not able to send this email")    
